d126f8580b0c3b0805fee23fb3181ac4c9f30ef9
Elene-tech <elenakurilets@gmail.com>
2022-08-18 14:55:05 +0200

    changed API key

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 let apiKey = "f4717a93880ad531595d505e3579e813";
-let city = "New York";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric;
-
+let currentCity = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=f4717a93880ad531595d505e3579e813&units=metric`;
+console.log(apiUrl);
