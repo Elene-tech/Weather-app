6fc3d738235314946c4c895d70252c769175e76c
Elene-tech <elenakurilets@gmail.com>
2022-08-23 22:09:36 +0200

    added additonal functionality
    added new icons, everyday forecast, change fahrenheit into celsius, implemented new api

--- src/app.js
+++ src/app.js
@@ -1,3 +1,55 @@
+const getData = async (lat, lon, units) => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const apiKey = "D7ZPSP23W6LGWCHAFMMP22J8T";
+  const unitGroup = units; // us = Fahrenheit, metric = Celcius
+
+  let startDate = new Date();
+  startDate.setDate(startDate.getDate() + 1);
+  startDate = startDate.toISOString().slice(0, 10);
+
+  let endDate = new Date();
+  endDate.setDate(endDate.getDate() + 5);
+  endDate = endDate.toISOString().slice(0, 10);
+
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/${startDate}/${endDate}?key=${apiKey}&unitGroup=${unitGroup}`;
+  const response = await axios.get(url);
+
+  if (response.hasOwnProperty("data")) {
+    let dayCounter = 1;
+    response.data.days.forEach((day) => {
+      const dayName = days[new Date(day.datetime).getDay()];
+      const minTemp = day.tempmin;
+      const maxTemp = day.tempmax;
+      const icon = day.icon;
+
+      const dayNameHTML = document.querySelector(`#day${dayCounter}`);
+      const temperatureMinHTML = document.querySelector(
+        `#temperature-day${dayCounter}-min`
+      );
+      const temperatureMaxHTML = document.querySelector(
+        `#temperature-day${dayCounter}-max`
+      );
+      let iconHTML = document.querySelector(`#icon${dayCounter}`);
+
+      iconHTML.setAttribute("src", `img/${icon}.png`);
+      dayNameHTML.innerHTML = dayName;
+      temperatureMinHTML.innerHTML = Math.round(minTemp);
+      temperatureMaxHTML.innerHTML = Math.round(maxTemp);
+
+      dayCounter = dayCounter + 1;
+    });
+  }
+};
+
 function formatTime() {
   let today = new Date().toLocaleTimeString([], {
     hour: "2-digit",
@@ -29,30 +81,13 @@ function formatDate() {
 const currentDate = document.querySelector(".current-day#day-current");
 currentDate.innerHTML = `${formatDate()}`;
 
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "f4717a93880ad531595d505e3579e813";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=f4717a93880ad531595d505e3579e813&units=metric`;
+function getForecast(coordinates, units) {
+  getData(coordinates.lat, coordinates.lon, units);
 }
 
-function displayTemperature(response) {
+function displayTemperature(response, units) {
   console.log(response);
-  let temperatureElement1 = document.querySelector(
-    ".current-day-temperature#temperature-current-day-index"
-  );
-  temperatureElement1.innerHTML = Math.round(response.data.main.temp);
 
-  celciusTemperature = response.data.main.temp;
-
-  let temperatureElement2 = document.querySelector(
-    ".tuesday-weather-temperature-day#temperature-tuesday-day"
-  );
-  temperatureElement2.innerHTML = Math.round(response.data.main.temp_max);
-
-  let temperatureElement3 = document.querySelector(
-    ".tuesday-weather-temperature-night#temperature-tuesday-night"
-  );
-  temperatureElement3.innerHTML = Math.round(response.data.main.temp_min);
   let weatherDescription = document.querySelector(
     ".current-visibility#visibility-description"
   );
@@ -67,6 +102,7 @@ function displayTemperature(response) {
     ".current-wind-index#index-wind-description"
   );
   wind.innerHTML = response.data.wind.speed;
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
 
@@ -76,21 +112,29 @@ function displayTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  let mainTemperature = document.querySelector(
+    "#temperature-current-day-index"
+  );
+  mainTemperature.innerHTML = Math.round(response.data.main.temp);
 
-  getForecast(response.data.coord);
+  getForecast(response.data.coord, units);
 }
 
-function search(city) {
+function search(city, units = "metric") {
   let apiKey = "f4717a93880ad531595d505e3579e813";
 
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f4717a93880ad531595d505e3579e813&units=metric`;
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f4717a93880ad531595d505e3579e813&units=${
+    units === "us" ? "imperial" : units
+  }`;
+  axios
+    .get(`${apiUrl}&appid=${apiKey}`)
+    .then((response) => displayTemperature(response, units));
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.value);
+  search(cityInputElement.value, "metric");
 }
 
 let form = document.querySelector("#search-form");
@@ -98,24 +142,19 @@ form.addEventListener("submit", handleSubmit);
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
-  let currentTemperatureElement = document.querySelector(
-    "#temperature-current-day-index"
-  );
-  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
-  currentTemperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  let cityInputElement = document.querySelector("#city");
+  search(cityInputElement.innerHTML, "us");
 }
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  let currentTemperatureElement = document.querySelector(
-    "#temperature-current-day-index"
-  );
-  currentTemperatureElement.innerHTML = Math.round(celciusTemperature);
+  let cityInputElement = document.querySelector("#city");
+  search(cityInputElement.innerHTML, "metric");
 }
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-let celciusTemperature = null;
-
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+getData(50.04, 19.94, "metric");
